8f42d58889008737cc6b8d2e9ff721621503ba17
Afari Richmond Addo <afaririchmond1000@gmail.com>
2024-03-22 00:34:29 +0000

    added the feature to change to next windoe using javascript

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-var SubmitBtn = document.querySelector("hero-btn");
+const nextBtn = document.getElementById("next");
+
+
+
+nextBtn.addEventListener("click", function() {
+    const nameInput = document.getElementById("name-input").value;
+    const emailInput = document.getElementById("email-input").value;
+    const passwordInput = document.getElementById("tel-input").value;
+
+    if (nameInput.trim() === "") {
+        alert("Please enter your name");
+    } else if (emailInput.trim() === "") {
+        alert("Please enter your email");
+    } else if (passwordInput.trim() === "") {
+        alert("Please enter your password");
+    } else {
+        window.location.href = "step2.html";
+    }
+});
